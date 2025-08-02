94f103621e2881ac1e0ee82aa503a87bf329aaa8
Ying Hsin <limyinghsin@gmail.com>
2025-08-02 15:23:00 +0800

    Initial Commit

---
+++ Challenge 2 App/Challenge_2_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2_AppApp.swift
+//  Challenge 2 App
+//
+//  Created by Lim Ying Hsin on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
