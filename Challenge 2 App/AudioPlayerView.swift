46a72b9300c04c759df7c0a6e48a08873ed8b224
xiuqi <summerlinsg@gmail.com>
2025-09-06 16:53:16 +0800

    made the tiles move

--- Challenge 2 App/AudioPlayerView.swift
+++ Challenge 2 App/AudioPlayerView.swift
@@ -9,12 +9,47 @@ import SwiftUI
 import Subsonic
 
 struct AudioPlayerView: View {
+    @State var offset: CGFloat = 0
+    let timer = Timer.publish(every: 60/100, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Button("stop") {
-            stop(sound:"sample audio.mp3")
+        GeometryReader { geometry in
+            VStack {
+                Button("move") {
+                    withAnimation(.linear(duration: 2)) {
+                        offset = 1
+                    }
+                }
+                HStack {
+                    Divider()
+                    Rectangle()
+                        .frame(width:150, height:180)
+                        .offset(y: (geometry.size.height+360) * offset - (geometry.size.height+360)/2)
+                    Divider()
+                    Rectangle()
+                        .frame(width:150, height:180)
+                        .offset(y: 50)
+                    Divider()
+                    Rectangle()
+                        .frame(width:150, height:180)
+                        .offset(y:-20)
+                    Divider()
+                    Rectangle()
+                        .frame(width:150, height:180)
+                        .offset(y:20)
+                    Divider()
+                }
+            }
+        }
+        .onReceive(timer) { _ in
+            offset = 0
+            withAnimation(.linear) {
+               offset = 1
+            }
         }
     }
 }
+
 
 
 
