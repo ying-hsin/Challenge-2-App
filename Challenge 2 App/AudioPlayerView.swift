92ad45b452c4433b22ee2fc0903719ad25b80b07
xiuqi <summerlinsg@gmail.com>
2025-09-08 13:34:24 +0800

    resolved tile glitch

--- Challenge 2 App/AudioPlayerView.swift
+++ Challenge 2 App/AudioPlayerView.swift
@@ -14,41 +14,51 @@ struct AudioPlayerView: View {
     @State var offsetB: CGFloat = 0
     @State var offsetC: CGFloat = 0
     @State var offsetD: CGFloat = 0
+    @State var offsetE: CGFloat = 0
     
     @State var showButtonA: Bool = true
     @State var showButtonB: Bool = true
     @State var showButtonC: Bool = true
     @State var showButtonD: Bool = true
+    @State var showButtonE: Bool = true
     
     @State private var tapCount: Int = 0
     
     @State var timeHasPassed: Bool = false
     
+    @State var storedVar = 0
+    
     let timer = Timer.publish(every: 60/100, on: .main, in: .common).autoconnect()
     func showA() {
         offsetA = 0
         showButtonA = true
-        withAnimation(.linear(duration:3)) {
+        withAnimation(.linear(duration:1)) {
             offsetA = 1
         }
     }
     func showB() {
         offsetB = 0
         showButtonB = true
-        withAnimation(.linear(duration:3)) {
+        withAnimation(.linear(duration:1)) {
             offsetB = 1
         }
     }
     func showC() {
         offsetC = 0
-        withAnimation(.linear(duration:3)) {
+        withAnimation(.linear(duration:1)) {
             offsetC = 1
         }
     }
     func showD() {
         offsetD = 0
-        withAnimation(.linear(duration:3)) {
+        withAnimation(.linear(duration:1)) {
             offsetD = 1
+        }
+    }
+    func showE() {
+        offsetE = 0
+        withAnimation(.linear(duration:1)) {
+            offsetE = 1
         }
     }
     
@@ -65,18 +75,16 @@ struct AudioPlayerView: View {
                         
                         Rectangle()
                             .fill(.black)
-                            .frame(width:150, height:180)
+                            .frame(width:130, height:180)
                             .onTapGesture {
                                 tapCount += 1
                                 showButtonA = false
                             }
                             .opacity(showButtonA == false ? 0:1)
                             .offset(y: (geometry.size.height+360) * offsetA - (geometry.size.height+360)/2)
-                        
-                        
                         Divider()
                         Rectangle()
-                            .frame(width:150, height:180)
+                            .frame(width:130, height:180)
                             .onTapGesture {
                                 tapCount += 1
                                 showButtonB = false
@@ -85,7 +93,7 @@ struct AudioPlayerView: View {
                             .offset(y: (geometry.size.height+360) * offsetB - (geometry.size.height+360)/2)
                         Divider()
                         Rectangle()
-                            .frame(width:150, height:180)
+                            .frame(width:130, height:180)
                             .onTapGesture {
                                 tapCount += 1
                                 showButtonC = false
@@ -94,13 +102,22 @@ struct AudioPlayerView: View {
                             .offset(y:(geometry.size.height+360) * offsetC - (geometry.size.height+360)/2)
                         Divider()
                         Rectangle()
-                            .frame(width:150, height:180)
+                            .frame(width:130, height:180)
                             .onTapGesture {
                                 tapCount += 1
                                 showButtonD = false
                             }
                             .opacity(showButtonD == false ? 0:1)
                             .offset(y:(geometry.size.height+360) * offsetD - (geometry.size.height+360)/2)
+                        Divider()
+                        Rectangle()
+                            .frame(width:130, height:180)
+                            .onTapGesture {
+                                tapCount += 1
+                                showButtonE = false
+                            }
+                            .opacity(showButtonE == false ? 0:1)
+                            .offset(y:(geometry.size.height+360) * offsetE - (geometry.size.height+360)/2)
                         Divider()
                     }
                 }
@@ -109,15 +126,21 @@ struct AudioPlayerView: View {
                 play(sound: "sample audio.mp3")
             }
             .onReceive(timer) { _ in
-                let randomInt = Int.random(in: 1..<5)
+                var randomInt = Int.random(in: 1..<6)
+                while randomInt == storedVar {
+                    randomInt = Int.random(in: 1..<6)
+                }
+                storedVar = randomInt
                 if randomInt == 1 {
                     showA()
                 } else if randomInt == 2 {
                     showB()
                 } else if randomInt == 3 {
                     showC()
-                } else {
+                } else if randomInt == 4 {
                     showD()
+                } else {
+                    showE()
                 }
             }
             .task {
